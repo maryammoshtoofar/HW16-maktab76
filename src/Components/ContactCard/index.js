bd8690969a900f587f6e8e7f73dbcafb7fc2bdfa
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2022-07-20 21:37:15 +0430

    add functionality

--- src/Components/ContactCard/index.js
+++ src/Components/ContactCard/index.js
@@ -1,25 +1,33 @@
 import React from "react";
 import "./index.css";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 class ContactCard extends React.Component {
   render() {
-    const { fname } = this.props.contact;
+    const { fname, familyName, email, relation } = this.props.contact;
+    console.log(this.props.contact);
 
     return (
       <>
-        <div className="card card-1">
-          <div className="card__icon">
-            <i className="fas fa-bolt"></i>
+        <div className="card">
+          <div className="cardInfo">
+            <div className="card-titles">
+              <div className="card__icons">
+                <div className="icon__link">
+                  <FaTrashAlt size={15} />
+                </div>
+                <div className="icon__link">
+                  <FaEdit size={15} />
+                </div>
+              </div>
+              <p className="card__title">
+                {fname} {familyName}{" "}
+              </p>
+            </div>
+            <h4 className="card__title">{relation} </h4>
+            <h4 className="card__title">{email} </h4>
           </div>
-          <p className="card__exit">
-            <i className="fas fa-times"></i>
-          </p>
-          <h2 className="card__title">{fname} </h2>
-          <p className="card__apply">
-            <a className="card__link" href="#">
-              Apply Now <i className="fas fa-arrow-right"></i>
-            </a>
-          </p>
         </div>
       </>
     );
